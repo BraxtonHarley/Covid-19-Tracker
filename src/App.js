eb1cce561f966454c5a4c3e565f15c5795ff7b54
Braxton Harley <braxton.2012.harley@gmail.com>
2021-01-30 11:04:29 -0600

    created dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+import { MenuItem, Select, FormControl } from '@material-ui/core';
 
 function App() {
   return (
     <div className="app">
       <h1>Covid 19 Tracker</h1>
+
+    <FormControl className="app_dropdown">
+      <Select
+        variant="outlined"
+        value="abc"
+        onChange="...">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Option 4</MenuItem>
+      </Select>
+    </FormControl>
+      
     </div>
   );
 }
